4c950dc031f286bb3ac51da1beab175f3d109a88
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-05-05 15:49:57 -0400

    hotel class updates

--- src/main/java/com/pluralsight/Hotel.java
+++ src/main/java/com/pluralsight/Hotel.java
@@ -1,8 +1,8 @@
 package com.pluralsight;
 
 public class Hotel {
-  private String name;
-  private int numberOfSuites, numberOfRooms, bookedSuites,bookedBasicRooms;
+    private String name;
+    private int numberOfSuites, numberOfRooms, bookedSuites, bookedBasicRooms;
 
     public Hotel(String name, int numberOfSuites, int numberOfRooms) {
         this.name = name;
@@ -16,5 +16,57 @@ public class Hotel {
         this.numberOfRooms = numberOfRooms;
         this.bookedSuites = bookedSuites;
         this.bookedBasicRooms = bookedBasicRooms;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfSuites() {
+        return numberOfSuites;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public int getBookedSuites() {
+        return bookedSuites;
+    }
+
+    public int getBookedBasicRooms() {
+        return bookedBasicRooms;
+    }
+
+    public int getAvailableSuites() {
+        return numberOfSuites - bookedSuites;
+
+
+    }
+
+    public int getAvailableRooms() {
+        return numberOfRooms - bookedBasicRooms;
+
+    }
+
+    public boolean bookRoom(int numberOfRooms, boolean isSuite) {
+        if (isSuite) {
+            if (numberOfRooms <= getAvailableSuites()) {
+                bookedSuites = bookedSuites + numberOfRooms;
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (numberOfRooms <= getAvailableRooms()) {
+                bookedBasicRooms = bookedBasicRooms + numberOfRooms;
+                return true;
+            } else {
+                return false;
+
+            }
+
+
+        }
     }
 }
